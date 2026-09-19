5ec4221d660a4db1759fcba612e99e50d0db6118
NicholasMa01 <nicholasmamusic@gmail.com>
2026-09-19 14:59:18 -0400

    SFX and Music Test
    sfx and music test

---
+++ Creative Jam_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_INGREDIENT_1 = 2114724127U;
+        static const AkUniqueID PLAY_INGREDIENT_2 = 2114724124U;
+        static const AkUniqueID PLAY_LOOPZEROPLAYLIST = 1246549400U;
+        static const AkUniqueID STOP_LOOPZEROPLAYLIST = 3061592398U;
+    } // namespace EVENTS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
