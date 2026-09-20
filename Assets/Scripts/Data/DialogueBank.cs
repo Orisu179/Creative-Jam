33b46f5541054177bb1f61ef8d092fb85d1bed77
Holly Hong <hollyhong@Amys-MacBook-Pro.local>
2026-09-19 22:14:46 -0400

    dialogue bank

--- Assets/Scripts/Data/DialogueBank.cs
+++ Assets/Scripts/Data/DialogueBank.cs
@@ -21,15 +21,15 @@ public static class DialogueBank
  
     public static Dictionary<Mood, string[]> greetings = new()
     {
-        { Mood.happy, new[] { "happy greeting 1" } },
-        { Mood.tired, new[] { "tired greeting 1" } },
-        { Mood.sunny, new[] { "sunny greeting 1" } },
-        { Mood.chilly, new[] { "chilly greeting 1" } }
+        { Mood.happy, new[] { "Hi! It's a great day! ", "Hey! How are you doing? ", "Hello! Love you see you again! " } },
+        { Mood.tired, new[] { "[yawn] Hi... ", "Ugh, it's too early for this... ", "Oh, me? Sorry, I'm not a morning person. "} },
+        { Mood.sunny, new[] { "It's another day of sun! ", "This sun... I can feel my feathers shriveling up already. ", "Thank god you have AC in here. "} },
+        { Mood.chilly, new[] { "Man, I should have worn a jacket today. ", "The wind's rough out there! Not a good day for flying. ", "Brr, is winter coming already? " } }
     };
 
     public static Dictionary<Mood, string[]> goodbyes = new()
     {
-        { Mood.happy, new[] { "happy goodbye 1" } },
+        { Mood.happy, new[] { "Thanks so much! Have a good one! ", "See you in the skies! ", "Hope to see you later! " } },
         { Mood.tired, new[] { "tired goodbye 1" } },
         { Mood.sunny, new[] { "sunny goodbye 1" } },
         { Mood.chilly, new[] { "chilly goodbye 1" } }
@@ -41,53 +41,53 @@ public static class DialogueBank
         { 
             Mood.happy, new() 
             {
-                { DrinkAttribute.fruity, new[] { "happy fruity hint 1" } },
-                { DrinkAttribute.sweet, new[] { "happy sweet hint 1" } },
-                { DrinkAttribute.strong, new[] { "happy strong hint 1" } },
-                { DrinkAttribute.magical, new[] { "happy magical hint 1" } },
-                { DrinkAttribute.cold, new[] { "happy cold hint 1" } },
-                { DrinkAttribute.bubbly, new[] { "happy bubbly hint 1" } },
-                { DrinkAttribute.hot, new[] { "happy hot hint 1" } },
-                { DrinkAttribute.milky, new[] { "happy milky hint 1" } }
+                { DrinkAttribute.fruity, new[] { "I'm feeling something fruity today. ", "Throw an extra lemon on there? ", "Do you have any strawberries back there? Or something like that? "} },
+                { DrinkAttribute.sweet, new[] { "I'd love a sweet treat today. ", "Don't hold back on the syrup.", "I'm craving some sugar. You gotta live a little sometimes, you know? " } },
+                { DrinkAttribute.strong, new[] { "Hit me with all you got! ", "I'm booked and busy today, so give me something strong! ", "I want a big kick! " } },
+                { DrinkAttribute.magical, new[] { "Put a little something special in there. ", "Could I get some glitter? ", "Surprise me with something out of this world! " } },
+                { DrinkAttribute.cold, new[] { "I need something to cool me down. ", "Extra ice, please! ", "Ice cold, please! " } },
+                { DrinkAttribute.bubbly, new[] { "Something carbonated, please. ", "Something sparkling sounds good today. ", "Give me something fizzy, please. " } },
+                { DrinkAttribute.hot, new[] { "I need something to warm me up. ", "Make it nice and toasty. ", "Gonna go curl up with a book after this. " } },
+                { DrinkAttribute.milky, new[] { "Do you have oat milk? Soy is fine, too. No? Okay, 2%. ", "Creamy like a dream, please. ", "My body might be lactose intolerant, but my heart disagrees. " } }
             }
         },
         { 
             Mood.tired, new() 
             {
-                { DrinkAttribute.fruity, new[] { "tired fruity hint 1" } },
-                { DrinkAttribute.sweet, new[] { "tired sweet hint 1" } },
-                { DrinkAttribute.strong, new[] { "tired strong hint 1" } },
-                { DrinkAttribute.magical, new[] { "tired magical hint 1" } },
-                { DrinkAttribute.cold, new[] { "tired cold hint 1" } },
-                { DrinkAttribute.bubbly, new[] { "tired bubbly hint 1" } },
-                { DrinkAttribute.hot, new[] { "tired hot hint 1" } },
-                { DrinkAttribute.milky, new[] { "tired milky hint 1" } }
+                { DrinkAttribute.fruity, new[] { "Something with fruit, [yawn], maybe... ", "Whatever fruit you have... ", "I think I need some Vitamin C... " } },
+                { DrinkAttribute.sweet, new[] { "Give me a sugar rush... " , "Extra sugar, please... I need it..... ", "Ugh, I need the glucose... " } },
+                { DrinkAttribute.strong, new[] { "I want my drink to take out a Victorian owlet. ", "Can I get a double shot? ...Maybe a triple? ", "As strong as you legally can.. " } },
+                { DrinkAttribute.magical, new[] { "Glitter? Sure, alright. I'll take all the help I can get... ", "By any chance do you know any necromancy? ", "Some of that... [waves claws vaguely], too..... " } },
+                { DrinkAttribute.cold, new[] { "Iced is fine. ", "Extra ice... ", "Ice cold. " } },
+                { DrinkAttribute.bubbly, new[] { "Carbonated, please... ", "Something fizzy.. ", "Maybe some bubbles would help..... " } },
+                { DrinkAttribute.hot, new[] { "Extra hot... ", "I need a hot mug in my claws.. ", "Warm would be good.. " } },
+                { DrinkAttribute.milky, new[] { "A splash of milk. ", "Lots of... [yawn].. cream..... ", "Extra foam.. " } }
             }
         },
         { 
             Mood.sunny, new() 
             {
-                { DrinkAttribute.fruity, new[] { "sunny fruity hint 1" } },
-                { DrinkAttribute.sweet, new[] { "sunny sweet hint 1" } },
-                { DrinkAttribute.strong, new[] { "sunny strong hint 1" } },
-                { DrinkAttribute.magical, new[] { "sunny magical hint 1" } },
-                { DrinkAttribute.cold, new[] { "sunny cold hint 1" } },
-                { DrinkAttribute.bubbly, new[] { "sunny bubbly hint 1" } },
-                { DrinkAttribute.hot, new[] { "sunny hot hint 1" } },
-                { DrinkAttribute.milky, new[] { "sunny milky hint 1" } }
+                { DrinkAttribute.fruity, new[] { "This weather's got me craving something tropical. ", "Something tart! ", "Something juicy, please. "} },
+                { DrinkAttribute.sweet, new[] { "Sweet like the sun. That doesn't really make sense, but you get me. ", "Extra sugar. " , "Extra sweet! " } },
+                { DrinkAttribute.strong, new[] { "Robust and full-bodied. ", "I want it to hit hard! ", "I'll take a double. "} },
+                { DrinkAttribute.magical, new[] { "A little golden hour shine would be perfect. ", "Squeeze in some sunshine for me. ", "Give me something exciting. " } },
+                { DrinkAttribute.cold, new[] { "Ice cold. ", "I'll definitely take this one iced. ", "Extra ice. " } },
+                { DrinkAttribute.bubbly, new[] { "Fizzy. ", "Make that sparkling. ", "With bubbles. " } },
+                { DrinkAttribute.hot, new[] { "Hot, please. My office is still freezing. ", "Hot like the weather. ", "Still hot. You know, cold drinks are bad for the body. My grandfeather always told me that. " } },
+                { DrinkAttribute.milky, new[] { "With some milk on top, please. ", "Lots of foam! ", "With a bit of cream. " } }
             }
         },
         { 
             Mood.chilly, new() 
             {
-                { DrinkAttribute.fruity, new[] { "chilly fruity hint 1" } },
-                { DrinkAttribute.sweet, new[] { "chilly sweet hint 1" } },
-                { DrinkAttribute.strong, new[] { "chilly strong hint 1" } },
-                { DrinkAttribute.magical, new[] { "chilly magical hint 1" } },
-                { DrinkAttribute.cold, new[] { "chilly cold hint 1" } },
-                { DrinkAttribute.bubbly, new[] { "chilly bubbly hint 1" } },
-                { DrinkAttribute.hot, new[] { "chilly hot hint 1" } },
-                { DrinkAttribute.milky, new[] { "chilly milky hint 1" } }
+                { DrinkAttribute.fruity, new[] { "I'm not ready for winter yet... Give me something fruity. ", "Do fruits grow in the winter..? I think we have to appreciate them while they're still here. ", "A fruity flavour sounds nice. " } },
+                { DrinkAttribute.sweet, new[] { "Something indulgent. ", "Extra sweet. ", "Any maple syrup? No? I'll take vanilla, then. "} },
+                { DrinkAttribute.strong, new[] { "Strong, please. ", "I need it running through my veins to make it to work today. ", "Give me the darkest thing you have. "} },
+                { DrinkAttribute.magical, new[] { "Maybe some of that Christmas magic, if you have it already? ", "I want it to feel like watching the first snow fall. ", ""} },
+                { DrinkAttribute.cold, new[] { "Cold like my heart. ", "Iced. My feathers are freezing either way. ", "Throw a clawful of snow in there. " } },
+                { DrinkAttribute.bubbly, new[] { "I think I'd fall asleep in the sky if I had warm milk now. ", "Fizzy, please. ", "Crisp and sparkling. "} },
+                { DrinkAttribute.hot, new[] { "Piping hot. In this weather I need it. ", "Extra hot! ", "I need this to warm my soul. " } },
+                { DrinkAttribute.milky, new[] { "Extra creamy. ", "Lots of milk.", "With milk. I'll leave it out tonight for Stork-a." } }
             }
         }
 };
