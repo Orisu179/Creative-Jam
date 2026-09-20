1509e9d71539c6ce94b561276fa68dce7840196c
Holly Hong <hollyhong@Amys-MacBook-Pro.local>
2026-09-20 14:05:32 -0400

    game over menu

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    // dont w about global state
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject fired;
+    [SerializeField] private GameObject maxLooped;
     void Start()
     {
-        
+        fired.SetActive(false);
+        maxLooped.SetActive(false);
+
+        if(GlobalFields.Instance != null)
+        {
+            if(GlobalFields.Instance.State == GlobalFields.GameOverState.Fired)
+            {
+                maxLooped.SetActive(false);
+                fired.SetActive(true);
+            }
+            else
+            {
+                fired.SetActive(false);
+                maxLooped.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log("it's null");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void returnToMain()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 }
