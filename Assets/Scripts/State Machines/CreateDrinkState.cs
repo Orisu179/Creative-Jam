86e0b2adc55d63a18bc709d0e8dd17f8e7727183
Eric Liu <eeric048@uottawa.ca>
2026-09-20 08:34:52 -0400

    disable menu button until create drink state

--- Assets/Scripts/State Machines/CreateDrinkState.cs
+++ Assets/Scripts/State Machines/CreateDrinkState.cs
@@ -31,6 +31,9 @@ namespace State_Machines
             _createDrink.OnDrinkCreated = HandleDrinkCreated;
             // 3. Enable the menu button
 
+            // enable button to open/close menu
+            _manager.DrinkMenu.EnableButton();
+
             // Test create drink
             _createDrink.drinkManager.ResetMix();
             _createDrink.drinkManager.AddIngredient(Ingredient.MILK);
@@ -46,6 +49,9 @@ namespace State_Machines
         public async Task Exit()
         {
             _createDrink.RemoveDrink();
+            // close and disable menu
+            _manager.DrinkMenu.CloseAndDisable();
+
             // TODO: Disable ingredients listeners
             // Ease out the create drink UI
             Tween fadeOut = _brewingSpriteManager.FadeOut();
